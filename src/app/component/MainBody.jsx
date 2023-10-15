fc45caee83cc6d7bd2b3187d72e0d526d38463c0
Aung Myo Myat <aungmyomyat@gicjp.com>
2023-10-15 08:50:09 +0630

    update randomNumber array shuffle

--- src/app/component/MainBody.jsx
+++ src/app/component/MainBody.jsx
@@ -69,6 +69,15 @@ const MainBody = () => {
         setAnswerForSelectedQuestion(filteredAnswer)
     };
 
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+    shuffleArray(randomNumbers);
+
     function myanmarToEnglishNumber(myanmarNumber) {
         if (typeof myanmarNumber !== 'string') {
             myanmarNumber = myanmarNumber.toString();
