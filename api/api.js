7df613411d4de496eb59b045313794d36681e7f6
aungmyomyat220 <aungmyomyat874@gmail.com>
2024-01-23 12:00:47 +0630

    update

--- api/api.js
+++ api/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-const API_BASE_URL = "http://localhost:5000";
 
 export const getQuestions = async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/questions`);
+        const response = await axios.get(`/questions`);
         return response.data;
     } catch (error) {
         console.error("Error getting questions:", error);
@@ -12,7 +11,7 @@ export const getQuestions = async () => {
 
 export const getAnswers = async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/answers`);
+        const response = await axios.get(`/answers`);
         return response.data;
     } catch (error) {
         console.error("Error getting answers:", error);
